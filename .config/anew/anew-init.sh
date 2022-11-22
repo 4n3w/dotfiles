7dc37218e584abc91d350be127f0d5930bab67ab
4n3w <4n3w@users.noreply.github.com>
2022-11-22 12:25:42 -0500

    Add aws helper via rob

---
+++ .config/anew/anew-init.sh
@@ -0,0 +1,33 @@
+export GPG_TTY=$(tty)
+
+alias k=kubectl
+alias t=tanzu
+
+alias gpra='git pull --rebase --autostash'
+alias gs='git status'
+
+kicktap(){
+    kctrl app kick -a tap -n tap-install
+}
+
+ktree(){
+    k tree -n $1 workload $2
+}
+
+events_in_namespace(){
+    k get events --sort-by=".lastTimestamp" -n $1
+}
+
+retap(){
+    tanzu package installed update tap -p tap.tanzu.vmware.com -v  --values-file tap-values.yaml -n tap-install && echo y | kicktap
+}
+
+awscreds(){
+    #!/bin/bash
+    declare awsCreds=`pbpaste | sed 's/ export//'`
+    declare output="[default]\n" # start with profile name, or adjust function to take a variable
+    for line in $awsCreds; do
+        output+="$line\n";
+    done
+    printf $output > ~/.aws/credentials
+}
