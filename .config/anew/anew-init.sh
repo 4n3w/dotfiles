c133c8131b307e993e96e79a13108990171b47d7
Andrew Wood <817945+4n3w@users.noreply.github.com>
2025-07-23 12:33:51 -0400

    Update anew-init.sh
    Remove old refs to keys/hostnames that are long since gone.

--- .config/anew/anew-init.sh
+++ .config/anew/anew-init.sh
@@ -66,18 +66,6 @@ uninstall_tap(){
  echo "y" | tanzu package installed delete tap -n tap-install
 }
 
-use_github(){
-  git config --global user.email '4n3w@users.noreply.github.com'
-  git config --global user.name '4n3w'
-  git config --global user.signingkey '68CDDFE4BB2A4007'
-}
-
-use_gitlab(){
-  git config --global user.email 'anwood@vmware.com'
-  git config --global user.name 'anwood'
-  git config --global user.signingkey '2C7B2B98DF652673'
-}
-
 change_git_author(){
   git commit --amend --author="Author Name <email@address.com>" --no-edit
 }
@@ -92,33 +80,6 @@ open_supply_chain(){
 yaml() {
   #VALUE=$(yaml ~/my_yaml_file.yaml "['a_key']")
   python3 -c "import yaml;print(yaml.safe_load(open('$1'))$2)"
-}
-
-defunct_tkgslogin(){
-    export KUBECTL_VSPHERE_PASSWORD='8V0=oZTZze3BksdwhDM'
-    export CLUSTER_NAMESPACE=tap-learn
-    kubectl vsphere login --server=https://vc01cl01-wcp.h2o-2-5037.h2o.vmware.com:6443 --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify --tanzu-kubernetes-cluster-name view-2 --tanzu-kubernetes-cluster-namespace ${CLUSTER_NAMESPACE}
-    kubectl vsphere login --server=https://vc01cl01-wcp.h2o-2-5037.h2o.vmware.com:6443 --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify --tanzu-kubernetes-cluster-name build --tanzu-kubernetes-cluster-namespace ${CLUSTER_NAMESPACE}
-    kubectl vsphere login --server=https://vc01cl01-wcp.h2o-2-5037.h2o.vmware.com:6443 --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify --tanzu-kubernetes-cluster-name run --tanzu-kubernetes-cluster-namespace ${CLUSTER_NAMESPACE}
-}
-
-tkgslogin(){
-    export KUBECTL_VSPHERE_PASSWORD='2qLPqOZrQZnmQHl0q$8'
-    export KUBECTL_VSPHERE_USERNAME='administrator@vsphere.local'
-    export CLUSTER_NAMESPACE=tap
-    export H2O_SLOT="h2o-2-6585"
-    H2O_KUBE_API="https://vc01cl01-wcp.$H2O_SLOT.h2o.vmware.com:6443"
-
-    local clusters=(shared-services view build build2 build3 run)
-
-    for cluster in ${clusters}; do
-      kubectl vsphere login \
-        --server=$H2O_KUBE_API \
-        --vsphere-username $KUBECTL_VSPHERE_USERNAME \
-        --insecure-skip-tls-verify \
-        --tanzu-kubernetes-cluster-name $cluster \
-        --tanzu-kubernetes-cluster-namespace ${CLUSTER_NAMESPACE}
-    done
 }
 
 use(){
