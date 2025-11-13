d6803f142f07cdbf3e92928cb922219f05c57c8d
Andrew Wood <4n3w@users.noreply.github.com>
2025-11-12 22:45:12 -0500

    Git helpers for things.

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -14,5 +14,16 @@ return {
     vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git blame' })
     vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>', { desc = 'Git diff' })
     vim.keymap.set('n', '<leader>gw', ':Gwrite<CR>', { desc = 'Git add current file' })
+    vim.keymap.set('n', '<leader>gw', ':Gwrite<CR>', { desc = 'Git add current file' })
+    vim.keymap.set('n', '<leader>gr', ':Git pull --rebase --autostash<CR>', { desc = 'Git pull rebase autostash' })
+    vim.keymap.set('n', '<leader>g+', ':Git stash pop<CR>', { desc = 'Git stash pop' })
+    vim.keymap.set('n', '<leader>g-', ':Git stash drop<CR>', { desc = 'Git stash drop' })
+    vim.api.nvim_create_user_command('GitAcceptTheirs', function()
+      vim.cmd('!git diff --name-only --diff-filter=U | xargs git checkout --theirs && git diff --name-only --diff-filter=U | xargs git add')
+      vim.cmd('Git') -- Refresh fugitive status
+    end, {})
+    vim.keymap.set('n', '<leader>gt', ':GitAcceptTheirs<CR>', { desc = 'Accept Theirs for all conflicts'})
+    vim.keymap.set('n', '<leader>gu', ':Git restore --staged %<CR>', { desc = 'Git unstage current file' })
+    vim.keymap.set('n', '<leader>gU', ':Git restore %<CR>', { desc = 'Git restore current file (discard changes)' })
   end,
 }
